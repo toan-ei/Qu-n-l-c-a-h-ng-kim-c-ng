ce796471d19ee33abb7e95745dcbea004808f0da
toan-ei <leductoanboyno2@gmail.com>
2024-12-05 22:31:09 +0700

    login and banggia

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('bosieutap', views.boSieuTap),
-    path('banggiakimcuong', views.bangGiaKimCuong, name="banggiakimcuong"),
+    path('banggiakimcuong', views.bangGiaKimCuong),
     path('login', views.login)
 ]
