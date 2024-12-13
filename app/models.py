ac5b222c303dfd945c1f589a41f68ee2fae508a8
toan-ei <leductoanboyno2@gmail.com>
2024-12-13 14:21:38 +0700

    new

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    userName = models.CharField(max_length=255)       
+    password = models.CharField(max_length=255) 
+
+    def __str__(self):
+        return self.userName
