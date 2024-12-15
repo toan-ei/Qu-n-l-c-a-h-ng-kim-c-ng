e8dfd5d33bd989c3915a9c0938c25699b808d9c7
Dang Van Tan <dangvantan47dl@gamil.com>
2024-12-15 12:05:27 +0700

    bo sung cho  phieubaohanh.html

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('banggiakimcuong', views.bangGiaKimCuong),
     path( 'login',views.loginPage, name="login"),
     path( 'register',views.register, name="register"),
-    path('phieubaohanh', views.phieubaohanh, name='phieubaohanh'),
+    path('phieubaohanh/', views.phieubaohanh, name='phieubaohanh'),
     path('sanpham', views.sanpham, name="sanpham"),
     path('phieutichdiem/', views.phieutichdiem, name='phieutichdiem'),
 ]
