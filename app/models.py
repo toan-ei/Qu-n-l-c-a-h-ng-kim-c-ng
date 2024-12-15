0743cfa77b3ea58d91f4139a01dea08241846747
toan-ei <leductoanboyno2@gmail.com>
2024-12-15 16:45:31 +0700

    database

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-from django.contrib.auth.forms import UserCreationForm
-#Change fomrs register django
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
-# Create your models here.
+# Tác giả
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
--- cuahangkimcuong/settings.py
+++ cuahangkimcuong/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'cuahangkimcuong.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoKimCuong', 
+        'USER': 'root', 
+        'PASSWORD': '1234',   
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',
     }
 }
 
