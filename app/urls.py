69f476903f7bf565b2d15dfe0e21292089f19b7c
Toann06 <minhtoan2005z@gmail.com>
2025-01-06 15:16:59 +0700

    thay đổi trang đăng kí, đăng nhập

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home),
     path('bosuutap', views.boSuuTap),
     path('banggiakimcuong', views.bangGiaKimCuong),
-    path( 'login',views.loginPage, name="login"),
+    path( 'login/',views.loginPage, name="login"),
     path( 'register',views.register, name="register"),
     path('phieubaohanh/', views.phieubaohanh, name='phieubaohanh'),
     path('sanpham', views.sanpham, name="sanpham"),
